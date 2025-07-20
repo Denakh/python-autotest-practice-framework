49756f79ce9f68090f22140cd447da56c9b4ca06
Denakh <denakhokh@gmail.com>
2025-07-20 21:39:18 +0300

    updated test

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 class TestUi:
 
     def test_something(self):
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         driver.get("https://www.google.com/")
         assert True
